57db4ad7818c4fa5966adb9c2561b4dc329e927b
Tanishq Kochar <tanishqkochar73@gmail.com>
2024-09-08 13:44:26 +0530

    Draft-2

--- app.js
+++ app.js
@@ -8,19 +8,6 @@ app .use(express.urlencoded({extended:true}));
 let publicPath = path.join(__dirname,"public");
 app.use(express.static(publicPath));
 
-app.set('view engine','ejs');
-
-app.get('/ejs',function(req,res){
-  // res.send(html);
-  res.render('hello.ejs',{
-    name:"Tanishq Kochar"
-  });
-})
-
-app.get('/contact', function (req,res){
-  res.render('contact.ejs');
-})
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- public/script.js
+++ public/script.js
@@ -1,1 +1,28 @@
-let ejs = require('ejs');
+let navbar = document.querySelector('.navbar');
+let body = document.querySelector('body');
+
+// body.onscroll = (ev)=>{
+//     const currentUrl = window.location;
+//     console.log(currentUrl)
+//     // console.log(window.scrollY)
+//     if(window.scrollY>500){
+//         navbar.style.backgroundColor = 'Black';
+//     }else{
+//         navbar.style.backgroundColor = 'White';
+//     }
+// }
+
+document.addEventListener('scroll',()=>{
+    const currentUrl = window.location;
+    console.log(currentUrl)
+    console.log(window.scrollY)
+    if(window.scrollY>1000){
+        navbar.style.backgroundColor = '#E0F7FA';
+    } else if(window.scrollY>2000){
+        navbar.style.backgroundColor = 'red';
+    }
+    else{
+        navbar.style.backgroundColor = 'White';
+    }
+    
+})
